35ab469bde11eb135a0efecad2f1e0a57a73cba7
dsarona <dsarona@users.noreply.github.com>
2018-08-11 12:26:03 -0700

    Updated files

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import include
+from django.contrib.auth import login, logout
+#from django.contrib.auth.views import login, logout
+#import django.contrib.auth.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url('', include('myblog.urls')),
+    url('login/', login, {'template_name': 'login.html'}, name="login"),
+    url('logout/', logout, {'next_page': '/'}, name="logout"),
 ]
